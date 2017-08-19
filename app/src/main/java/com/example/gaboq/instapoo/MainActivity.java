66398516a7eacb9dcb4f6a0688be292bef0cca06
Gabriel Quesada <gaboq98@gmail.com>
2017-08-19 12:48:10 -0600

    Base del GUI

---
+++ app/src/main/java/com/example/gaboq/instapoo/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.gaboq.instapoo;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import layout.GalleryFragment;
+import layout.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    transaction.replace(R.id.content, new MainFragment()).commit();
+                    return true;
+
+                case R.id.navigation_gallery:
+                    transaction.replace(R.id.content, new GalleryFragment()).commit();
+                    return true;
+
+                case R.id.navigation_cam:
+                    //Aqui va la camara
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.content, new MainFragment()).commit();
+    }
+
+}
