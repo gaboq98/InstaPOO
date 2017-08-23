3db0c4971c67cba7808882556c17d32f5a4ef438
Gabriel Quesada <gaboq98@gmail.com>
2017-08-23 00:54:10 -0600

    Camara mala

--- app/src/main/java/com/example/gaboq/Cam/CamActivity.java
+++ app/src/main/java/com/example/gaboq/Cam/CamActivity.java
@@ -11,17 +11,17 @@ import android.provider.MediaStore;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
-
-import com.example.gaboq.instapoo.MainFactory;
 import com.example.gaboq.instapoo.R;
-import com.example.gaboq.instapoo.filters.IFilter;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
+
+import static java.io.File.createTempFile;
 
 public class CamActivity extends AppCompatActivity {
 
@@ -39,43 +39,48 @@ public class CamActivity extends AppCompatActivity {
 
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
+
     public void takePhoto(View view) {
         Intent callCamera = new Intent();
         callCamera.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-        File photoFile = null;
-        try {
-            photoFile = createImageFile();
-        } catch (IOException e){
-            e.printStackTrace();
+        if (callCamera.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                ///e.printStackTrace();
+                Log.e("takePhoto", "IOException", e);
+                return;
+            }
+
+            if (photoFile != null) {
+                callCamera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                startActivityForResult(callCamera, START_CAMERA_APP);
+            }
         }
-        callCamera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
-        startActivityForResult(callCamera, START_CAMERA_APP);
     }
 
     protected void onActivityResult (int requestCode, int resultCode, Intent data) {
 
         if (requestCode == START_CAMERA_APP && resultCode == RESULT_OK) {
 
-            //Bundle extras = data.getExtras();
-            //Bitmap bitmap = (Bitmap) extras.get("data");
-            //MainFactory mFactory = new MainFactory();
-            //IFilter f = mFactory.getFilter(bitmap, 1);
-            //bitmap = f.generateBitmap();
             Bitmap bitmap = BitmapFactory.decodeFile(mImageLocation);
             mPhotoCapture.setImageBitmap(bitmap);
 
         }
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
+
     File createImageFile() throws IOException {
 
-        String time = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String time = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            time = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        }
         String imageFileName = "IMAGE" + time + "_";
         File storageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
 
-        File imageFile = File.createTempFile(imageFileName, ".jpg", storageDirectory);
+        File imageFile = createTempFile(imageFileName, ".jpg", storageDirectory);
         mImageLocation = imageFile.getAbsolutePath();
 
         return imageFile;
