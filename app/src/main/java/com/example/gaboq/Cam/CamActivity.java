477783545aa4f28577c75952017b44372cd7ba28
Gabriel Quesada <gaboq98@gmail.com>
2017-08-22 22:50:15 -0600

    Nada osea basura innonorar

--- app/src/main/java/com/example/gaboq/Cam/CamActivity.java
+++ app/src/main/java/com/example/gaboq/Cam/CamActivity.java
@@ -2,10 +2,15 @@ package com.example.gaboq.Cam;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -14,11 +19,15 @@ import com.example.gaboq.instapoo.MainFactory;
 import com.example.gaboq.instapoo.R;
 import com.example.gaboq.instapoo.filters.IFilter;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
 public class CamActivity extends AppCompatActivity {
 
     private ImageView mPhotoCapture;
-
     private static final int START_CAMERA_APP = 0;
+    private String mImageLocation = "";
 
 
     @Override
@@ -30,9 +39,17 @@ public class CamActivity extends AppCompatActivity {
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public void takePhoto(View view) {
         Intent callCamera = new Intent();
         callCamera.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+        File photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        callCamera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
         startActivityForResult(callCamera, START_CAMERA_APP);
     }
 
@@ -40,15 +57,28 @@ public class CamActivity extends AppCompatActivity {
 
         if (requestCode == START_CAMERA_APP && resultCode == RESULT_OK) {
 
-            Bundle extras = data.getExtras();
-            Bitmap bitmap = (Bitmap) extras.get("data");
-            MainFactory mFactory = new MainFactory();
-            IFilter f = mFactory.getFilter(bitmap, 1);
-            bitmap = f.generateBitmap();
+            //Bundle extras = data.getExtras();
+            //Bitmap bitmap = (Bitmap) extras.get("data");
+            //MainFactory mFactory = new MainFactory();
+            //IFilter f = mFactory.getFilter(bitmap, 1);
+            //bitmap = f.generateBitmap();
+            Bitmap bitmap = BitmapFactory.decodeFile(mImageLocation);
             mPhotoCapture.setImageBitmap(bitmap);
 
-
         }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    File createImageFile() throws IOException {
+
+        String time = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "IMAGE" + time + "_";
+        File storageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+        File imageFile = File.createTempFile(imageFileName, ".jpg", storageDirectory);
+        mImageLocation = imageFile.getAbsolutePath();
+
+        return imageFile;
     }
 
 }
