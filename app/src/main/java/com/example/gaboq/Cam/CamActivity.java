79b2b8adff82425df1266cdbee3af5b65d600cb1
Gabriel Quesada <gaboq98@gmail.com>
2017-08-22 20:06:55 -0600

    Sirve pero feo

--- app/src/main/java/com/example/gaboq/Cam/CamActivity.java
+++ app/src/main/java/com/example/gaboq/Cam/CamActivity.java
@@ -1,9 +1,14 @@
 package com.example.gaboq.Cam;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+
 
 import com.example.gaboq.instapoo.R;
 
@@ -11,6 +16,7 @@ public class CamActivity extends AppCompatActivity {
 
     private ImageView mPhotoCapture;
 
+    private static final int START_CAMERA_APP = 0;
 
 
     @Override
@@ -22,5 +28,22 @@ public class CamActivity extends AppCompatActivity {
 
     }
 
+    public void takePhoto(View view) {
+        Intent callCamera = new Intent();
+        callCamera.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(callCamera, START_CAMERA_APP);
+    }
+
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == START_CAMERA_APP && resultCode == RESULT_OK) {
+
+            Bundle extras = data.getExtras();
+            Bitmap bitmap = (Bitmap) extras.get("data");
+            mPhotoCapture.setImageBitmap(bitmap);
+
+
+        }
+    }
 
 }
--- app/src/main/java/com/example/gaboq/instapoo/MainActivity.java
+++ app/src/main/java/com/example/gaboq/instapoo/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -22,8 +23,6 @@ import layout.MainFragment;
 public class MainActivity extends AppCompatActivity {
 
     public static final int SELECT_PHOTO_ACTION = 0;
-
-    private static final int START_CAMERA_APP = 0;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -43,31 +42,14 @@ public class MainActivity extends AppCompatActivity {
 
                 case R.id.navigation_cam:
                     //Aqui va la camara
+                    Intent intent = new Intent(MainActivity.this, CamActivity.class);
+                    startActivity(intent);
                     return true;
             }
             return false;
         }
 
     };
-
-    public void takePhoto(MenuItem item) {
-        Intent callCamera = new Intent();
-        callCamera.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(callCamera, START_CAMERA_APP);
-    }
-
-    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
-
-        if (requestCode == START_CAMERA_APP && resultCode == RESULT_OK) {
-
-            Bundle extras = data.getExtras();
-            Bitmap bitmap = (Bitmap) extras.get("data");
-
-            Intent intent = new Intent(MainActivity.this, CamActivity.class);
-            startActivity(intent);
-        }
-    }
-
 
 
 
@@ -82,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.content, new MainFragment()).commit();
+
     }
 
 
