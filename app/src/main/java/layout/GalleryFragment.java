cc0799a784a7ba6813cf18f83c81ae220a984ca9
Josué Canales <josuecanales0@gmail.com>
2017-08-24 02:35:17 -0600

    Version Alpha
    Aun falta implementa la funcion a la hora de presionar un boton

--- app/src/main/java/layout/GalleryFragment.java
+++ app/src/main/java/layout/GalleryFragment.java
@@ -1,23 +1,82 @@
 package layout;
 
+import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 import com.example.gaboq.instapoo.R;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class GalleryFragment extends Fragment {
+    GridView gv;
+    ArrayList<File> list;
+
     public GalleryFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_gallery, container, false);
+        View v = inflater.inflate(R.layout.fragment_gallery, container, false);
+        list = imageReader(Environment.getExternalStorageDirectory());
+
+        gv = (GridView) v.findViewById(R.id.gridView);
+        gv.setAdapter(new GridAdapter());
+
+        return v;
+    }
+
+    class GridAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = getActivity().getLayoutInflater().inflate(R.layout.single_grid, parent ,false);
+            ImageView iv = (ImageView) convertView.findViewById(R.id.imageView2);
+            iv.setImageURI(Uri.parse( getItem(position).toString() ));
+            return convertView;
+        }
+    }
+
+    ArrayList<File> imageReader(File root){
+        ArrayList<File> a = new ArrayList<>();
+        File[] files = root.listFiles();
+
+        for (int i = 0; i < files.length; i++) {
+            if(files[i].isDirectory()){
+                a.addAll(imageReader(files[i]));
+            }else{
+                if(files[i].getName().endsWith(".jpg")){
+                    a.add(files[i]);
+                }
+            }
+        }
+        return a;
     }
 
 }
