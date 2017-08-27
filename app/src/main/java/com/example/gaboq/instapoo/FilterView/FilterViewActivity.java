9ccbfbaede437c82410dd4c871ebccd12cba0516
Josué Canales <josuecanales0@gmail.com>
2017-08-26 18:15:00 -0600

    pruebas

--- app/src/main/java/com/example/gaboq/instapoo/FilterView/FilterViewActivity.java
+++ app/src/main/java/com/example/gaboq/instapoo/FilterView/FilterViewActivity.java
@@ -6,6 +6,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -17,6 +18,7 @@ import com.example.gaboq.instapoo.R;
 import com.example.gaboq.instapoo.filters.IFilter;
 
 import java.io.File;
+import java.io.IOException;
 
 public class FilterViewActivity extends AppCompatActivity {
 
@@ -32,7 +34,7 @@ public class FilterViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter_view);
 
-        String i = getIntent().getStringExtra("img");
+        final String i = getIntent().getStringExtra("img");
 
 
         simpleGallery = (Gallery) findViewById(R.id.simpleGallery);
@@ -47,8 +49,15 @@ public class FilterViewActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //Aqui sucede la magia
-                Drawable d = getResources().getDrawable(images[position]);
-                Bitmap bitmap = ((BitmapDrawable)d).getBitmap();
+                //Drawable d = getResources().getDrawable(images[position]);
+                //Bitmap bitmap = ((BitmapDrawable)d).getBitmap();
+                Bitmap bitmap = null;
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(FilterViewActivity.this.getContentResolver(), Uri.parse(i));
+                } catch (IOException e) {
+                    Drawable d = getResources().getDrawable(images[position]);
+                    bitmap = ((BitmapDrawable)d).getBitmap();
+                }
                 bitmap = Bitmap.createScaledBitmap(bitmap, 640, 480, true);
                 MainFactory mFactory = new MainFactory();
                 IFilter f = mFactory.getInstance(bitmap, position + 1);
